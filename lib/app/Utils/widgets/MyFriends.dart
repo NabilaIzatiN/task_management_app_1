2636e13c58fbde1ad5c69720a58e89389e8d75c7
Nabila <nabila.izatin22.12.2000@gmail.com>
2023-01-01 14:09:59 +0700

    Video (7.5))

--- lib/app/Utils/widgets/MyFriends.dart
+++ lib/app/Utils/widgets/MyFriends.dart
@@ -1,15 +1,13 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:ionicons/ionicons.dart';
 import 'package:task_management_app_1/app/Utils/styles/AppColors.dart';
-import 'package:task_management_app_1/app/data/controller/auth.controller.dart';
 import 'package:task_management_app_1/app/routes/app_pages.dart';
 
-import 'header.dart';
-
 class MyFriend extends StatelessWidget {
-final authCon = Get.find<AuthController>();
+  const MyFriend({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -23,10 +21,8 @@ final authCon = Get.find<AuthController>();
                 children: [
                   const Text(
                     'My Friends',
-                    style: TextStyle(
-                      color: AppColors.primaryText,
-                      fontSize: 30,
-                    ),
+                    style:
+                        TextStyle(color: AppColors.primaryText, fontSize: 25),
                   ),
                   const Spacer(),
                   GestureDetector(
@@ -48,57 +44,37 @@ final authCon = Get.find<AuthController>();
               ),
               SizedBox(
                 height: 400,
-                child: StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
-                  stream: authCon.streamFriends(),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const Center(child: CircularProgressIndicator());
-                    }
-
-                    var myFriends = (snapshot.data!.data()
-                        as Map<String, dynamic>)['emailFriends'] as List;
-
-                    return GridView.builder(
-                      shrinkWrap: true,
-                      itemCount: myFriends.length,
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: context.isPhone ? 2 : 3,
-                          crossAxisSpacing: 20,
-                          mainAxisSpacing: 40),
-                      itemBuilder: (context, index) {
-                        return StreamBuilder<
-                                DocumentSnapshot<Map<String, dynamic>>>(
-                            stream: authCon.streamUsers(myFriends[index]),
-                            builder: (context, snapshot2) {
-                              if (snapshot2.connectionState ==
-                                  ConnectionState.waiting) {
-                                return const Center(
-                                    child: CircularProgressIndicator());
-                              }
-
-                              var data = snapshot2.data!.data();
-
-                              return Column(
-                                children: [
-                                  ClipRRect(
-                                    borderRadius: BorderRadius.circular(50),
-                                    child: Image(
-                                        image: NetworkImage(data!['photo']),
-                                      height: Get.width * 0.35,
-                                      width: Get.width * 0.4,
-                                      fit: BoxFit.cover,
-                                    ),
-                                  ),
-                                  Text(
-                                    data['name'],
-                                    style: const TextStyle(
-                                      color: AppColors.primaryText,
-                                    ),
-                                  ),
-                                ],
-                              );
-                            });
-                      },
-                    );
-                  },
-                ),
+                child: GridView.builder(
+                    shrinkWrap: true,
+                    itemCount: 8,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 3,
+                            crossAxisSpacing: 20,
+                            mainAxisSpacing: 40),
+                    itemBuilder: (context, index) {
+                      return Column(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(100),
+                            child: const Image(
+                                image: NetworkImage(
+                                    'https://pbs.twimg.com/profile_images/1539609458514358272/VeuA18MI_400x400.jpg',
+                                    scale: 3)),
+                          ),
+                          const Spacer(),
+                          const Text(
+                            'Daffi Fadillah',
+                            style: TextStyle(color: AppColors.primaryText),
+                          ),
+                        ],
+                      );
+                    }),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
